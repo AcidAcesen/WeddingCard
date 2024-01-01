23af12066f2028cbe2fb0c6f21d98995fb86e18a
MUHAMAD FADIL AIMAN BIN MOHD ZIN <119920064+AcidAcesen@users.noreply.github.com>
2024-01-01 15:03:36 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var backgroundSound = document.getElementById("backgroundSound");
-  
-    backgroundSound.play();
-  
-    backgroundSound.addEventListener("ended", function () {
-      this.currentTime = 0;
-      this.play();
-    });
+  var backgroundSound = document.getElementById("backgroundSound");
+
+  document.addEventListener("click", function () {
+      backgroundSound.play();
   });
-  
+
+  backgroundSound.addEventListener("ended", function () {
+    this.currentTime = 0;
+    this.play();
+  });
+});
